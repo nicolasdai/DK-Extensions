ec80c0cb721cc7ead47147bcd6cbe6f487c33afb
baozi <gmxjian@gmail.com>
2021-06-30 15:44:53 +0800

    golang

--- Packages/com.dk.excelreader/tools/generate_csharp.sh
+++ Packages/com.dk.excelreader/tools/generate_csharp.sh
@@ -3,3 +3,4 @@ export outputPath=$1
 export protoPath=$2
 
 protoc --proto_path=$protoPath --csharp_out=$outputPath $protoPath/*.proto
+protoc --proto_path=$protoPath --go_out=/Users/mac/StaticData $protoPath/*.proto
